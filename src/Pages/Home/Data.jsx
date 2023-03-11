8ba02fc634723ed297cfbc8ee0291ddf5f4472a8
Tuong Thai <thaiphamngoctuong@gmail.com>
2023-03-12 03:22:18 +0700

    update

--- src/Pages/Home/Data.jsx
+++ src/Pages/Home/Data.jsx
@@ -57,8 +57,7 @@ const Data = () => {
       </h1>
       <h3 className="home__subtitle">Full-Stack Developer</h3>
       <p className="home__description">
-        I'm creative designer based in New York, and I'm very passionate and
-        dedicated to my work.
+        Mình tên thật là Thái Phạm Ngọc Tường. Mình có một niềm đam mê bất với việc gõ code.
       </p>
       <a href="#contact" className="button button--flex">
         Say Hello
