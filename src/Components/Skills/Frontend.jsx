5e75f2b21d6fea201f9c29a04c1d755179fe0bc3
Thai Pham Ngoc Tuong <thaiphamngoctuong@gmail.com>
2023-04-25 18:09:31 +0700

    update

--- src/Components/Skills/Frontend.jsx
+++ src/Components/Skills/Frontend.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
+import NextJS from "../../assets/nextjs.svg";
 
 const Frontend = () => {
-  const [skillArr, setArr] = useState([
-    { icon: <i className="bx bxl-html5"></i>, skillName: "HTML" },
-    { icon: <i className="bx bxl-css3"></i>, skillName: "CSS" },
+  const skillArr = [
     { icon: <i className="bx bxl-javascript"></i>, skillName: "JavaScript" },
+    { icon: <i className="bx bxl-react"></i>, skillName: "ReactJS" },
+    { icon: <i className="bx bxl-redux"></i>, skillName: "Redux" },
+    {
+      icon: <img className="skill__icons" alt="" src={NextJS} />,
+      skillName: "NextJS",
+    },
+    { icon: <i className="bx bxl-typescript"></i>, skillName: "TypeScript" },
+    { icon: <i className="bx bxl-sass"></i>, skillName: "SASS" },
     { icon: <i className="bx bxl-bootstrap"></i>, skillName: "Bootstrap" },
     { icon: <i className="bx bxl-tailwind-css"></i>, skillName: "TailwindCSS" },
-    { icon: <i className="bx bxl-git"></i>, skillName: "Git" },
-    { icon: <i className="bx bxl-sass"></i>, skillName: "SASS" },
     { icon: <i className="bx bx-checkbox-square"></i>, skillName: "AntDesign" },
-    { icon: <i className="bx bxl-react"></i>, skillName: "ReactJS" },
-    { icon: <i className="bx bxl-typescript"></i>, skillName: "TypeScript" },
     { icon: <i className="bx bx-code-alt"></i>, skillName: "Styled Component" },
-  ]);
+  ];
 
   let renderSkills = () => {
     return skillArr.map((skill, index) => {
