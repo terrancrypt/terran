12b44c358c159f4b7e685f1754b3655d02258a69
Thai Pham Ngoc Tuong <thaiphamngoctuong@gmail.com>
2023-06-11 20:33:34 +0700

    update about image

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import AboutImg from "../../assets/profile.jpg";
+import AboutImg from "../../assets/mepic2.jpg";
 import CV from "../../assets/cv-thai-pham-ngoc-tuong.pdf";
 import Info from "./Info";
 
