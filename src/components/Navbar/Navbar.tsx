85bf189bd8bbf9fc926068bd222b926bd36c72e0
Terran Thai <thaiphamngoctuong@gmail.com>
2023-09-13 21:32:08 +0700

    update navbar for test on mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,19 +11,19 @@ const Navbar = () => {
   return (
     <div
       className={`${
-        isOpen ? "translate-x-[400px] w-full" : ""
-      } fixed top-0 -left-[400px] h-full z-50 transition-transform`}
+        isOpen ? "translate-x-[70vw] sm:translate-x-[300px] w-full" : ""
+      } fixed top-0 -left-[70vw] sm:-left-[300px] h-full z-50 transition-transform`}
     >
       <div className="w-full h-full flex">
-        <div className="w-[300px] sm:w-[400px]  h-full bg-[#f8f8f8] dark:bg-[#1c1c1c] relative ">
+        <div className="w-[70vw] sm:w-[300px] h-full bg-[#f8f8f8] dark:bg-[#1c1c1c] relative">
           <button
             onClick={() => dispatch(toggleNavbar())}
-            className="absolute top-4 right-4"
+            className="absolute top-10 right-8"
           >
-            <XMarkIcon className="w-5 h-5" />
+            <XMarkIcon className="w-6 h-6" />
           </button>
         </div>
-        <div className="w-full h-full bg-[#ffffff] opacity-50"></div>
+        <div className="flex-grow h-full bg-[#ffffff] opacity-50"></div>
       </div>
     </div>
   );
